86c5b331411fbd44ef12361614802d00a988c14f
Chukwuma Mishael <chukwumamishael@yahoo.com>
2023-12-20 14:19:18 +0300

    Create Form

---
+++ DataBase2-project/Guest.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace DataBase2_project
+{
+    partial class Guest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            Submit = new Button();
+            pictureBox1 = new PictureBox();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(81, 122);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Guest Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(81, 161);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(267, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(520, 122);
+            label2.Name = "label2";
+            label2.Size = new Size(110, 28);
+            label2.TabIndex = 0;
+            label2.Text = "Guest SSN";
+            label2.Click += this.label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(520, 161);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(269, 31);
+            textBox2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(81, 268);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 28);
+            label3.TabIndex = 0;
+            label3.Text = "Guest Phone";
+            label3.Click += this.label3_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(81, 309);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(280, 31);
+            textBox3.TabIndex = 1;
+            textBox3.TextChanged += this.textBox3_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(520, 268);
+            label4.Name = "label4";
+            label4.Size = new Size(109, 28);
+            label4.TabIndex = 0;
+            label4.Text = "Guest Age";
+            label4.Click += this.label4_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(520, 307);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(269, 31);
+            textBox4.TabIndex = 1;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // Submit
+            // 
+            Submit.Location = new Point(364, 416);
+            Submit.Name = "Submit";
+            Submit.Size = new Size(166, 60);
+            Submit.TabIndex = 2;
+            Submit.Text = "Submit";
+            Submit.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.employee;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(88, 68);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(106, 48);
+            label5.Name = "label5";
+            label5.Size = new Size(175, 32);
+            label5.TabIndex = 4;
+            label5.Text = "Guest Reserve";
+            // 
+            // Guest
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkGreen;
+            ClientSize = new Size(856, 569);
+            Controls.Add(label5);
+            Controls.Add(pictureBox1);
+            Controls.Add(Submit);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Guest";
+            Text = "Guest";
+            Load += this.Guest_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Button Submit;
+        private PictureBox pictureBox1;
+        private Label label5;
+    }
+}
